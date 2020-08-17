0529df37d88b6370f69bc196a75c3343b6f76869
David Kim <66704969+zkffhtm6523@users.noreply.github.com>
2020-08-17 17:54:19 +0900

    2020.08.17 쉬퀀스 생성 및 콜백 함수

--- Oracle DB/example/20.08.17(BoardWeb4).sql
+++ Oracle DB/example/20.08.17(BoardWeb4).sql
@@ -3,10 +3,19 @@ create table t_user(
     user_id varchar2(30) not null,
     user_pw varchar2(100) not null,
     nm varchar2(20) not null,
-    mail varchar2(40),
+    email varchar2(40),
     profile_img varchar2(50),
     r_dt date default sysdate,
     m_dt date default sysdate
 );
+drop table t_user;
+select * from t_user;
+
+--시퀀스는 시작하자마자 20이 증가되어있어서, 컴퓨터 캐쉬 기능 꺼야함.
+create sequence seq_user
+nocache;
+drop SEQUENCE seq_user;
+commit;
 
 select * from t_user;
+delete from t_user;
